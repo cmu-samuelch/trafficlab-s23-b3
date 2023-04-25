41585f4c2b22effd9dacbfef5e4cc9e1fcf90098
cmu-samuelch <103439400+cmu-samuelch@users.noreply.github.com>
2023-04-25 00:18:41 -0400

    documentation
    added header comments to all functions, inline comments where necessary.

--- trafficlab_final.ino
+++ trafficlab_final.ino
@@ -13,7 +13,8 @@
 /* declare the NeoPixel Pixel object */
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPin, NP_type);
 
-/* typedef'ing the lane queue structure.
+/* @brief Lane queue structure, implemented as a singly-linked list.
+ *
  * I will store the lane queue as a circular singly-linked list of lanes,
  * where `queue` will always point to the last lane in the queue (NULL if
  * empty). To access the top of the queue, we access queue->next; to insert
@@ -28,20 +29,19 @@ typedef struct ll_ele {
 
 int car_count[NUMLANES] = {0,0,0,0};
 bool occupied_lanes[NUMLANES] = {false,false,false,false};
-int f_lanes[NUMLANES] = {A5,A2,A1,A0};
+int f_lanes[NUMLANES] = {A5,A2,A1,A0};    // set the pin for each lane
 int exit_lane = A4;
 
-int threshold[NUMLANES] = {90,50,50,50};
+int threshold[NUMLANES] = {90,50,50,50};  // thresholds for each lane.
 
 int current_green;
 lane_t *queue;
-unsigned long lane_green_time;
-unsigned long last_car_time;
+unsigned long lane_green_time, last_car_time;
 
 const unsigned long yellow_time = 1500;
 const unsigned long red_time = 1000;
 const unsigned long max_green_time = 30000; // max green time: 30s
-const unsigned long lane_empty_threshold = 1000; // time to treat lane as empty: 1.0s
+const unsigned long lane_empty_threshold = 1000; // time to treat lane as empty
 
 
 void setup() {
@@ -51,7 +51,7 @@ void setup() {
   // put your setup code here, to run once:
   queue = NULL;
   current_green = -1;
-  last_car_time = 0;  
+  last_car_time = 0;
   for (int i = 0; i < NUMLANES; i++) {
     occupied_lanes[i] = false;
     car_count[i] = 0;
@@ -68,22 +68,28 @@ void loop() {
   update_counts(sensor_data);
 
   load_lane_data(sensor_data);
-  if (set_green(sensor_data)) return;
+  if (set_green()) return;
 
   if (queue != NULL) set_red();
 }
 
+/* flashes all lights blue, then sets all lights to red. */
 void set_all_red(void) {
-  for (int i = 0; i < NUMLANES; i++)
+  for (int i = 0; i < NUMLANES; i++) // set all lanes to blue for 1s
     pixels.setPixelColor(i, pixels.Color(0,0,200));
-  pixels.show();
-  delay(1000);
+  pixels.show(); delay(1000);
+
   for (int i = 0; i < NUMLANES; i++)
     pixels.setPixelColor(i, pixels.Color(150, 0, 0));
   pixels.show();
-  Serial.println("initialized. set to red.");
+  Serial.println("initialized. set to red."); // then set all to red
 }
 
+/* reads raw sensor data for each lane, then records whether each lane has a 
+ * car present in `sensor_data`.
+ *
+ * INPUT sensor_data Array containing lane occupancies.
+ */
 void record_lane_data(bool *sensor_data) {
   int f_lane_vals[NUMLANES];
   /* this loop records the data into an array containing values
@@ -91,27 +97,44 @@ void record_lane_data(bool *sensor_data) {
    */
   for (int i = 0; i < NUMLANES; i++) {
     f_lane_vals[i] = map(analogRead(f_lanes[i]), 0, 1023, 0, 100);
-    /* sensor_data[i] is set to whether there is a car over that sensor) */
+    /* sensor_data[i] is set to whether there is a car over lane i's sensor */
     sensor_data[i] = (f_lane_vals[i] > threshold[i]);
-    Serial.print("lane "); Serial.print(i); Serial.println(f_lane_vals[i]);
   }
 }
 
+/* checks whether a car has left the lane on this check; if so increments the
+ * corresponding lane's car count.
+ *
+ * INPUT sensor_data Array containing lane occupancies.
+ */
 void update_counts(bool *sensor_data) {
   /* if a car is currently in the current lane, reset the timer since last car
    * detected in open lane
    */
   if (sensor_data[current_green])
     last_car_time = millis();
-  /* if a car was in this lane at last check but is no longer in the lane,
-   * increment the current lane's car count by 1.
+  
+  /* if a car was in current lane at last check but is no longer in the lane,
+   * increment current lane's car count by 1.
    */
-  if (!sensor_data[current_green] && occupied_lanes[current_green])
+  if (!sensor_data[current_green] && occupied_lanes[current_green]) {
     car_count[current_green] = car_count[current_green] + 1;
+    Serial.print("lane "); Serial.print(current_green);
+    Serial.print(": car count "); Serial.println(car_count[current_green]);
+  }
 }
 
+/* After reading sensor data into the `sensor_data` array, determines which (if
+ * any) lanes ought to be added to queue, and adds them. Does a bunch of queue
+ * stuff, elaborated upon in the lane_t header comment.
+ *
+ * INPUT sensor_data Array containing lane occupancies.
+ */
 void load_lane_data(bool *sensor_data) {
   for (int i = 0; i < NUMLANES; i++) {
+    /* if lane `i` was not occupied at last check but is now, and lane `i` is
+     * not the currently-green lane, add it to queue.
+     */
     if (!occupied_lanes[i] && sensor_data[i] && i != current_green) {
       /* add lane `i` to the queue. see line 14. */
       lane_t *next = queue;
@@ -120,16 +143,30 @@ void load_lane_data(bool *sensor_data) {
       if (queue == NULL) // make new_lane circular with itself
         new_lane->next = new_lane;
       else { // insert new_lane after the queue's tail
-        new_lane->next = queue->next;
+        new_lane->next = queue->next; // queue head remains lane following tail
         queue->next = new_lane;
       }
       queue = new_lane; // new_lane becomes the queue's new tail
     }
+    /* no matter what the sensor data was, we want to update occupied_lanes to
+     * have the same lane occupancies from current check. we do this here since
+     * we no longer need to compare the difference between current data and
+     * previous data.
+     */
     occupied_lanes[i] = sensor_data[i];
   }
 }
 
-bool set_green(bool *sensor_data) {
+/* Sets a green light, if applicable.
+ * 
+ * First checks if there is no current lane and that there is a lane in queue.
+ * If both of these are satisfied, then we remove the lane at the top of the
+ * queue, and set `current_green` to whatever that lane is. We then manage the
+ * queue to maintain its structure so it is still a valid lane queue.
+ *
+ * RETURN VALUE: `true` if we set a lane to green, `false` otherwise.
+ */
+bool set_green(void) {
   // if there is no current lane, set that lane
   if (current_green == -1 && queue != NULL) {
     // dequeue top of queue; set current_green to that lane
@@ -143,28 +180,31 @@ bool set_green(bool *sensor_data) {
 
     pixels.setPixelColor(current_green, pixels.Color(0, 150, 0));
     pixels.show();
-    lane_green_time = millis();
+    lane_green_time = millis(); // reset time since a lane was made green
     return true;
   }
   return false;
 }
 
-/* header comment */
+/* Checks whether a lane should be set to red, and does so if applicable.
+ * 
+ * The current lane should be set to red if either:
+ * I.   it has been green for longer than the green-threshold
+ * II.  it has been empty for longer than the empty-threshold
+ * If either of these is satisfied, then we start the yellow cycle. Note that
+ * since there are delays built into the yellow and red cycles, car-counting
+ * cannot occur during this time, and the car count may suffer error from this.
+ * Cars leaving the lane while the light is yellow cannot be counted, and will
+ * need to be accounted for manually.
+ */
 void set_red(void) {
   unsigned long current_time = millis();
 
-  /* if we exceed the max green time on the current lane, cycle the light
-   * to red.
-   *
-   * otherwise, check if the currently-green lane is empty.
-   * We accomplish this by checking if the time between now and the most recent
-   * car to pass through exceeds a certain time, say, 3 seconds.
-   * if we find that lane has been empty for `lane_empty_threshold` ms, cycle
-   * the light to red.
+  /* compare time elapsed since light set to green to green-threshold
+   * and time elapsed since last car present in lane to empty-threshold;
+   * if either of these times are above threshold then set current lane to
+   * yellow and then red.
    */
-  Serial.print("current lane "); Serial.println(current_green);
-  Serial.print("time since car"); Serial.println(current_time - last_car_time);
-  Serial.print("time since green"); Serial.println(current_time - lane_green_time);
   if (current_time - lane_green_time >= max_green_time
    || current_time - last_car_time >= lane_empty_threshold) {
     pixels.setPixelColor(current_green,  pixels.Color(150, 150, 0)); // Yellow
